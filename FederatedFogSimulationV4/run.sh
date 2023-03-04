bfb4824f8645438339e74c739c5725bb32d9ef1a
Lucas Noronha <lucasnoronha7777@gmail.com>
2023-03-04 17:55:32 -0300

    Adicionando script de simulação em lote

---
+++ FederatedFogSimulationV4/run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+run_simulation () {
+    docker compose up -d
+
+    previous_log=""
+    while true; do
+        log=`docker logs -n 1 data`
+
+        if [ "$log" != "$previous_log" ]; then
+            echo $log
+            previous_log=$log
+        fi
+
+        if echo $log | grep "Data saved" -q ;  then
+            docker compose down
+            break
+        fi
+
+        sleep 0.1
+    done
+}
+
+input_file="run_configs.txt"
+
+echo "" > .env
+
+while read line; do
+    if [ -z "${line}" ]; then
+        run_simulation
+        echo "" > .env
+    else
+        echo $line >> .env  
+    fi
+done < $input_file
+
+echo $line >> .env  
+
+run_simulation
