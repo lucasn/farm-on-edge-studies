cd8751990f7f0004860f1d1783378260bf20ae94
Lucas Noronha <lucasnoronha7777@gmail.com>
2023-03-21 15:46:22 -0300

    Gerando gráficos para mensagens respondidas pelas fogs e pela cloud

--- FederatedFogSimulationV4/figure_generation/generate_aggregate.py
+++ FederatedFogSimulationV4/figure_generation/generate_aggregate.py
@@ -18,14 +18,29 @@ def main():
     for config in configs:
         config['with_auction_response_time_mean'] = []
         config['without_auction_response_time_mean'] = []
+        config['with_auction_resp_time_mean_by_cloud'] = []
+        config['with_auction_resp_time_mean_by_fogs'] = []
+        config['without_auction_resp_time_mean_by_cloud'] = []
+        config['without_auction_resp_time_mean_by_fogs'] = []
 
         for data in config['with_auction_data']:
             config['with_auction_response_time_mean'].append(data['response_time'].mean()['response_time'])
 
+            resolved_by_cloud_with_auction = data['response_time'][data['response_time'].resolved_by_cloud == 1]
+            resolved_by_fogs_with_auction = data['response_time'][data['response_time'].resolved_by_cloud == 0]
+
+            config['with_auction_resp_time_mean_by_cloud'].append(resolved_by_cloud_with_auction['response_time'].mean())
+            config['with_auction_resp_time_mean_by_fogs'].append(resolved_by_fogs_with_auction['response_time'].mean())
+
         for data in config['without_auction_data']:
             config['without_auction_response_time_mean'].append(data['response_time'].mean()['response_time'])
 
-    
+            resolved_by_cloud_with_auction = data['response_time'][data['response_time'].resolved_by_cloud == 1]
+            resolved_by_fogs_with_auction = data['response_time'][data['response_time'].resolved_by_cloud == 0]
+            
+            config['without_auction_resp_time_mean_by_cloud'].append(resolved_by_cloud_with_auction['response_time'].mean())
+            config['without_auction_resp_time_mean_by_fogs'].append(resolved_by_fogs_with_auction['response_time'].mean())
+
     for config in configs:
         config['with_auction_response_time_confidence_interval'] = calculate_confidence_interval(
             sample=config['with_auction_response_time_mean'],
@@ -34,19 +49,47 @@ def main():
         config['without_auction_response_time_confidence_interval'] = calculate_confidence_interval(
             sample=config['without_auction_response_time_mean'],
             confidence_level=0.95
-        )    
+        )
+        config['with_auction_resp_time_cloud_conf_interval'] = calculate_confidence_interval(
+            sample=config['with_auction_resp_time_mean_by_cloud'],
+            confidence_level=0.95
+        )
+        config['with_auction_resp_time_fogs_conf_interval'] = calculate_confidence_interval(
+            sample=config['with_auction_resp_time_mean_by_fogs'],
+            confidence_level=0.95
+        )
+        config['without_auction_resp_time_cloud_conf_interval'] = calculate_confidence_interval(
+            sample=config['without_auction_resp_time_mean_by_cloud'],
+            confidence_level=0.95
+        )
+        config['without_auction_resp_time_fogs_conf_interval'] = calculate_confidence_interval(
+            sample=config['without_auction_resp_time_mean_by_fogs'],
+            confidence_level=0.95
+        )
     
     y_with_auction = []
     y_without_auction = []
+    y_with_auction_cloud = []
+    y_with_auction_fogs = []
+    y_without_auction_cloud = []
+    y_without_auction_fogs = []
     for config in configs:
         y_with_auction.append((config['with_auction_response_time_confidence_interval'][0] + config['with_auction_response_time_confidence_interval'][1])/2)
+        y_with_auction_cloud.append((config['with_auction_resp_time_cloud_conf_interval'][0] + config['with_auction_resp_time_cloud_conf_interval'][1])/2)
+        y_with_auction_fogs.append((config['with_auction_resp_time_fogs_conf_interval'][0] + config['with_auction_resp_time_fogs_conf_interval'][1])/2)
         y_without_auction.append((config['without_auction_response_time_confidence_interval'][0] + config['without_auction_response_time_confidence_interval'][1])/2)
+        y_without_auction_cloud.append((config['without_auction_resp_time_cloud_conf_interval'][0] + config['without_auction_resp_time_cloud_conf_interval'][1])/2)
+        y_without_auction_fogs.append((config['without_auction_resp_time_fogs_conf_interval'][0] + config['without_auction_resp_time_fogs_conf_interval'][1])/2)
 
     barwidth = 0.2
-    fig = plt.subplots(figsize =(12, 8))
+    fig = plt.subplots(figsize=(12, 8))
 
     confidence_interval_with_auction = [config['with_auction_response_time_confidence_interval'][1] - y for config, y in zip(configs, y_with_auction)]
+    confidence_interval_with_auction_cloud = [config['with_auction_resp_time_cloud_conf_interval'][1] - y for config, y in zip(configs, y_with_auction_cloud)]
+    confidence_interval_with_auction_fogs = [config['with_auction_resp_time_fogs_conf_interval'][1] - y for config, y in zip(configs, y_with_auction_fogs)]
     confidence_interval_without_auction = [config['without_auction_response_time_confidence_interval'][1] - y for config, y in zip(configs, y_without_auction)]
+    confidence_interval_without_auction_cloud = [config['without_auction_resp_time_cloud_conf_interval'][1] - y for config, y in zip(configs, y_without_auction_cloud)]
+    confidence_interval_without_auction_fogs = [config['without_auction_resp_time_fogs_conf_interval'][1] - y for config, y in zip(configs, y_without_auction_fogs)]
 
     indice = np.arange(len(y_with_auction))
     x1 = [x - barwidth for x in indice]
@@ -81,7 +124,74 @@ def main():
     )
 
     plt.legend()
-    plt.savefig(f'aggregate/{filename}.png')
+    plt.savefig(f'aggregate/{filename}_all.png')
+
+    plt.figure()
+
+    plt.bar(
+        x1, 
+        y_with_auction_fogs, 
+        color='r', 
+        width=1.5*barwidth, 
+        label='Com leilão',
+        edgecolor='black',
+        capsize=10,
+        yerr=confidence_interval_with_auction_fogs
+    )
+    plt.bar(
+        x2, 
+        y_without_auction_fogs, 
+        color='b', 
+        width=1.5*barwidth, 
+        label='Sem leilão',
+        edgecolor='black',
+        capsize=10,
+        yerr=confidence_interval_without_auction_fogs
+    )
+
+    plt.xlabel('Configuração do experimento')
+    plt.ylabel('Tempo de Resposta Médio (s)')
+    plt.xticks(
+        [r for r in range(len(y_with_auction))],
+        [f'{config["QUANTITY_FOGS"]} fogs / {config["QUANTITY_CLIENTS"]} clientes' for config in configs]
+    )
+
+    plt.legend()
+    plt.savefig(f'aggregate/{filename}_fogs.png')
+
+    plt.figure()
+
+    plt.bar(
+        x1, 
+        y_with_auction_cloud, 
+        color='r', 
+        width=1.5*barwidth, 
+        label='Com leilão',
+        edgecolor='black',
+        capsize=10,
+        yerr=confidence_interval_with_auction_cloud
+    )
+    plt.bar(
+        x2, 
+        y_without_auction_cloud, 
+        color='b', 
+        width=1.5*barwidth, 
+        label='Sem leilão',
+        edgecolor='black',
+        capsize=10,
+        yerr=confidence_interval_without_auction_cloud
+    )
+
+    plt.xlabel('Configuração do experimento')
+    plt.ylabel('Tempo de Resposta Médio (s)')
+    plt.xticks(
+        [r for r in range(len(y_with_auction))],
+        [f'{config["QUANTITY_FOGS"]} fogs / {config["QUANTITY_CLIENTS"]} clientes' for config in configs]
+    )
+
+    plt.legend()
+    plt.savefig(f'aggregate/{filename}_cloud.png')
+
 
     auction_performed_json = []
     for config in configs:
